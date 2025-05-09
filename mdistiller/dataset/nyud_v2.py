80eeb64eb6a79ec16d07dd6bdf1a437d19e60f4d
nda111 <geunhyeok@khu.ac.kr>
2025-05-09 08:06:25 +0000

    On branch dev-lineval-nyud-ddp Changes to be committed: 	modified:   mdistiller/dataset/nyud_v2.py 	modified:   tools/lineval/nyud.py
    Untracked files:
    	configs/yy/

--- mdistiller/dataset/nyud_v2.py
+++ mdistiller/dataset/nyud_v2.py
@@ -30,8 +30,7 @@ def denormalize(x: torch.Tensor):
     std = torch.tensor(STD, **tensor_metadata).reshape(stat_shape)
     return x * std + mean
 
-def get_nyud_train_transform(mean=MEAN, std=STD, img_size: int=224):
-    resizing_size = int(img_size * (256 / 224))
+def get_nyud_train_transform(mean=MEAN, std=STD):
     return A.Compose([
         A.HorizontalFlip(p=0.5),
         A.ColorJitter(
@@ -42,15 +41,14 @@ def get_nyud_train_transform(mean=MEAN, std=STD, img_size: int=224):
             p=0.5),
         A.Normalize(mean=mean, std=std),
         ToTensorV2(),
-    ], additional_targets={'depth': 'image'})
+    ], additional_targets={'depth': 'mask'})
 
 
-def get_nyud_test_transform(mean=MEAN, std=STD, img_size: int=224):
-    resizing_size = int(img_size * (256 / 224))
+def get_nyud_test_transform(mean=MEAN, std=STD):
     return A.Compose([
         A.Normalize(mean=mean, std=std),
         ToTensorV2(),
-    ], additional_targets={'depth': 'image'})
+    ], additional_targets={'depth': 'mask'})
 
 
 class NYUdV2(Dataset):
@@ -75,7 +73,7 @@ class NYUdV2(Dataset):
         
         output = self.transform(image=image, depth=depth)
         image: np.ndarray = output['image']
-        depth: np.ndarray = output['depth'][0]
+        depth: np.ndarray = output['depth']
         
         if return_masks:
             return image, depth, depth > 0
@@ -83,17 +81,16 @@ class NYUdV2(Dataset):
             return image, depth
 
 
-def get_nyud_val_loader(val_batch_size, use_ddp, mean=MEAN, std=STD, img_size: int=224):
-    test_transform = get_nyud_test_transform(mean, std, img_size=img_size)
+def get_nyud_val_loader(val_batch_size, use_ddp, mean=MEAN, std=STD):
+    test_transform = get_nyud_test_transform(mean, std)
     test_set = NYUdV2(split='test', transform=test_transform)
     test_loader = make_loader(test_set, val_batch_size, num_workers=16, shuffle=False, use_ddp=use_ddp)
     return test_loader
 
-def get_nyud_dataloaders(batch_size, val_batch_size, num_workers, use_ddp,
-    mean=MEAN, std=STD, img_size: int=224):
-    train_transform = get_nyud_train_transform(mean, std, img_size=img_size)
+def get_nyud_dataloaders(batch_size, val_batch_size, num_workers, use_ddp, mean=MEAN, std=STD):
+    train_transform = get_nyud_train_transform(mean, std)
     train_set = NYUdV2(split='train', transform=train_transform)
     num_data = len(train_set)
     train_loader = make_loader(train_set, batch_size, num_workers, shuffle=True, use_ddp=use_ddp)
-    test_loader = get_nyud_val_loader(val_batch_size, use_ddp, mean, std, img_size=img_size)
+    test_loader = get_nyud_val_loader(val_batch_size, use_ddp, mean, std)
     return train_loader, test_loader, num_data
--- tools/lineval/nyud.py
+++ tools/lineval/nyud.py
@@ -1,12 +1,15 @@
-from argparse import ArgumentParser
+import os
+from argparse import ArgumentParser, Namespace
 from tqdm.auto import tqdm
+from typing import Literal
 import numpy as np
 
 import einops
 
 import torch
-from torch import optim
+from torch import nn, optim
 from torch.optim import lr_scheduler
+from torch import distributed as dist
 from torchvision.transforms.functional import (
     resize, center_crop
 )
@@ -20,27 +23,91 @@ from tools.lineval.utils import (
     prepare_lineval_dir,
     load_from_checkpoint,
 )
+from mdistiller.utils import dist_fn
 
 
-if __name__ == '__main__':
-    parser = ArgumentParser('lineval.nyud')
-    init_parser(parser, defaults=dict(epochs=1000))
-    args = parser.parse_args()
+# Utility
+def crop_resize(*x: tuple[torch.Tensor, ...], size: int, random_crop: bool=False):
+    x_repr = x[0]
+    shorter = min(x_repr.shape[-2:])
     
-    DEVICE = args.device
+    x_cropped = [center_crop(x_, shorter) for x_ in x]
+    if random_crop:
+        crop_size = int(shorter * 0.8)
+        low = np.random.randint(0, shorter - crop_size - 1, size=(2,))
+        high = low + crop_size
+        x_cropped = [
+            x_[..., low[0]:high[0], low[1]:high[1]]
+            for x_ in x_cropped
+        ]
+    x_resized = [resize(x_, (size, size)) for x_ in x_cropped]
+    return x_resized
+
+def patch_to_depth(
+    x: torch.Tensor, 
+    size: tuple[int, int], 
+    head: nn.Linear, 
+):
+    '''
+    x: torch.Tensor.shape(B, (h, w), C)
+    ret: torch.Tensor.shape(B, H, W, D)
+    '''
+    num_rows = int(x.size(1)**0.5)
+    num_patches = num_rows * num_rows
+    x = x[..., -num_patches:, :]
+    x = einops.rearrange(x, 'B (h w) C -> B C h w', h=num_rows)
+    x_resized = resize(x, size=size)
+    x_resized = x_resized.permute(0, 2, 3, 1)  # B H W C
+    x_depth = head.forward(x_resized)  # B H W D
+    return x_depth
+
+def reform_depth(
+    x: torch.Tensor,
+    kind: Literal['logit', 'depth']='logit',
+):
+    '''
+    x: torch.Tensor.shape(B, H, W, D)
+    ret: 
+        logit: torch.Tensor.shape(B, H, W, D)
+        depth: torch.Tensor.shape(B, H, W)
+    '''
+    match kind:
+        case 'logit':
+            x = x.softmax(dim=-1)
+            x = x.permute(0, 3, 1, 2)
+        case 'depth':
+            resolution = x.size(-1)
+            depth_list = torch.linspace(0, 1, resolution, dtype=x.dtype, device=x.device)
+            depth_list = depth_list.reshape(1, 1, 1, resolution)
+            x_hard = nn.functional.one_hot(
+                x.argmax(dim=-1),
+                num_classes=resolution,
+            )
+            x = x_hard - x.detach() + x
+            x = x.sum(dim=-1)
+        case _:
+            raise NotImplementedError(kind)
+    return x
+
+    
+def main(args: Namespace):
+    rank = int(os.environ['LOCAL_RANK'])
+    IS_MASTER = bool(int(os.environ['IS_MASTER_NODE']))
+    DEVICE = rank
     EPOCHS = args.epochs
     
-    log_dir, log_filename, best_filename, last_filename = prepare_lineval_dir(
-        args.expname, 
-        tag=args.tag, 
-        dataset='nyud', 
-        args=vars(args)
-    )
+    if IS_MASTER:
+        _, log_filename, best_filename, last_filename = prepare_lineval_dir(
+            args.expname, 
+            tag=str(args.tag), 
+            dataset='nyud', 
+            args=vars(args)
+        )
     
     # DataLoaders, Models
     train_loader, test_loader, _ = get_nyud_dataloaders(
-        args.batch_size, args.test_batch_size,
-        args.num_workers, use_ddp=False,
+        args.batch_size//world_size, args.test_batch_size//world_size,
+        args.num_workers, use_ddp=True,
     )
     if args.timm_model is not None:
         print(f"Loading {args.timm_model} from timm")
@@ -48,7 +115,11 @@ if __name__ == '__main__':
     else:
         model, _ = load_from_checkpoint(args.expname, tag=args.tag)
     model: ModelBase = model.cuda(DEVICE)
-    head = torch.nn.Linear(model.embed_dim, 256).cuda(DEVICE)
+    depth_resolution = 256
+    head = torch.nn.Linear(model.embed_dim, depth_resolution).cuda(DEVICE)
+    
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[rank], find_unused_parameters=True)
+    head = nn.parallel.DistributedDataParallel(head, device_ids=[rank])
     optimizer = optim.SGD(
         head.parameters(),
         lr=args.learning_rate,
@@ -60,87 +131,74 @@ if __name__ == '__main__':
         eta_min=1.0E-8,
     )
     
-    # Utility
-    def crop_resize(x: torch.Tensor, size: int, random_crop: bool=False):
-        shorter = min(x.shape[-2:])
-        x_cropped = center_crop(x, shorter)
-        if random_crop:
-            crop_size = int(shorter * 0.8)
-            low = np.random.randint(0, shorter - crop_size - 1, size=(2,))
-            high = low + crop_size
-            x_cropped = x[..., low[0]:high[0], low[1]:high[1]]
-        x_resized = resize(x_cropped, (size, size))
-        return x_resized
-
-    def restore_depth_map(x: torch.Tensor, size: tuple[int, int]):
-        return resize(x, size)
-
-    def patches_to_depth(x: torch.Tensor):
-        _, num_patches, embed_dim = x.shape
-        patch_rows = int(num_patches**0.5)
-        pixel_rows = int(embed_dim**0.5)
-        return einops.rearrange(
-            x[:, -patch_rows*patch_rows:],
-            'b (ph pw) (h w) -> b (ph h) (pw w)',
-            ph=patch_rows, pw=patch_rows,
-            h=pixel_rows, w=pixel_rows,
-        )
-    
     # Training Loop
     best_rmse = torch.inf
     train_loss_list, train_rmse_list, test_loss_list, test_rmse_list = [], [], [], []
     for epoch in range(args.epochs):
         
-        with tqdm(train_loader, desc=f'TRAIN {epoch+1}', dynamic_ncols=True) as bar:
+        with tqdm(train_loader, desc=f'TRAIN {epoch+1}', dynamic_ncols=True, disable=not IS_MASTER) as bar:
             total_loss, total_rmse, total = 0, 0, 0
             for input, target in bar:
-                input = crop_resize(input, size=224, random_crop=True)
+                input, = crop_resize(input, size=224, random_crop=True)
                 with torch.no_grad():
-                    x = model.forward_stem(input.cuda(DEVICE))
-                    x = model.get_layers().forward(x)
-                pred_patches = head.forward(x)
-                pred = patches_to_depth(pred_patches)
-                pred = restore_depth_map(pred, target.shape[-2:])
+                    x = model.forward(input.cuda(DEVICE))[1]['feats'][-1]
+                pred = patch_to_depth(x, size=target.shape[-2:], head=head)
+                pred_logit = reform_depth(pred, kind='logit')
                 
-                loss = torch.nn.functional.mse_loss(pred, target.to(DEVICE))
+                target_binned = (target * (depth_resolution - 1)).round().long()
+                loss = torch.nn.functional.cross_entropy(pred_logit, target_binned.to(DEVICE), reduction='mean')
                 loss.backward()
+                if IS_MASTER: print(head.module.weight.grad.norm(), f'{loss:.8f}')
                 optimizer.step()
                 optimizer.zero_grad()
                 scheduler.step()
+                # if IS_MASTER: print(head.module.weight.grad.norm(), f'{loss:.8f}')
                 
-                batch_size = input.size(0)
-                total_loss += loss.clone().detach().cpu().item() * batch_size
-                total_rmse += torch.square(pred - target.to(DEVICE)).flatten(1) \
-                    .mean(dim=1).sqrt().sum().clone().detach().cpu().item()
-                total += batch_size
-                bar.set_postfix(dict(
-                    loss=total_loss/total,
-                    rmse=total_rmse/total,
-                    lr=optimizer.param_groups[0]['lr'],
-                ))
+                with torch.no_grad():
+                    pred_depth = reform_depth(pred, kind='depth')
+                    loss = torch.nn.functional.cross_entropy(pred_logit, target_binned.to(DEVICE), reduction='none')
+                    loss_all = dist_fn.gather(loss)
+                    local_rmse = torch.square(pred_depth - target.to(DEVICE)).flatten(1).mean(dim=1).sqrt()
+                    rmse_all = dist_fn.gather(local_rmse)
+                    
+                    total_loss += loss_all.mean().cpu().item()
+                    total_rmse += rmse_all.sum().cpu().item()
+                    total += loss_all.size(0)
+                    
+                    if IS_MASTER:
+                        bar.set_postfix(dict(
+                            lr=optimizer.param_groups[0]['lr'],
+                            loss=total_loss/total,
+                            rmse=total_rmse/total,
+                        ))
             train_rmse = total_rmse / total
             train_loss = total_loss / total
 
-        with tqdm(test_loader, desc=f' TEST {epoch+1}', dynamic_ncols=True) as bar, torch.no_grad():
-            total_loss, total = 0, 0
+        with tqdm(test_loader, desc=f' TEST {epoch+1}', dynamic_ncols=True, disable=not IS_MASTER) as bar, torch.no_grad():
+            total_loss, total_rmse, total = 0, 0, 0
             for input, target in bar:
-                input = crop_resize(input, size=224, random_crop=False)
-                x = model.forward_stem(input.cuda(DEVICE))
-                x = model.get_layers().forward(x)
-                pred_patches = head.forward(x)
-                pred = patches_to_depth(pred_patches)
-                pred = restore_depth_map(pred, target.shape[-2:])
-                loss = torch.nn.functional.mse_loss(pred, target.to(DEVICE))
+                input, target = crop_resize(input, target, size=224, random_crop=False)
+                x = model.forward(input.cuda(DEVICE))[1]['feats'][-1]
+                pred = patch_to_depth(x, size=target.shape[-2:], head=head)
+                pred_logit = reform_depth(pred, kind='logit')
+                pred_depth = reform_depth(pred, kind='depth')
                 
-                batch_size = input.size(0)
-                total_loss += loss.cpu().item() * batch_size
-                total_rmse += torch.square(pred - target.to(DEVICE)).flatten(1) \
-                    .mean(dim=1).sqrt().sum().cpu().item()
-                total += batch_size
-                bar.set_postfix(dict(
-                    loss=total_loss/total,
-                    rmse=total_rmse/total,
-                ))
+                target_binned = (target * (depth_resolution - 1)).round().long()
+                loss = torch.nn.functional.cross_entropy(pred_logit, target_binned.to(DEVICE), reduction='none')
+                
+                loss_all = dist_fn.gather(loss)
+                local_rmse = torch.square(pred_depth - target.to(DEVICE)).flatten(1).mean(dim=1).sqrt()
+                rmse_all = dist_fn.gather(local_rmse)
+                
+                total_loss += loss_all.mean().cpu().item()
+                total_rmse += rmse_all.sum().cpu().item()
+                total += loss_all.size(0)
+                
+                if IS_MASTER:
+                    bar.set_postfix(dict(
+                        loss=total_loss/total,
+                        rmse=total_rmse/total,
+                    ))
             test_rmse = total_rmse / total
             test_loss = total_loss / total
         
@@ -150,26 +208,46 @@ if __name__ == '__main__':
         test_loss_list.append(test_loss)
         test_rmse_list.append(test_rmse)
         
-        with open(log_filename, 'a') as file:
-            print(f'- epoch: {epoch+1}', file=file)
-            print(f'  train_loss: {train_loss:.4f}', file=file)
-            print(f'  train_rmse: {train_rmse:.4f}', file=file)
-            print(f'  test_loss: {test_loss:.4f}', file=file)
-            print(f'  test_rmse: {test_rmse:.4f}', file=file)
-            print(file=file)
-        
-        ckpt = dict(
-            epoch=epoch+1,
-            train_loss=train_loss_list,
-            train_rmse=train_rmse_list,
-            test_loss=test_loss_list,
-            test_rmse=test_rmse_list,
-            head={
-                key: val.clone().detach().cpu()
-                for key, val in head.state_dict().items()
-            },
-        )
-        if test_rmse > best_rmse:
-            best_rmse = test_rmse
-            torch.save(ckpt, str(best_filename))
-        torch.save(ckpt, str(last_filename))
+        if IS_MASTER:
+            with open(log_filename, 'a') as file:
+                print(f'- epoch: {epoch+1}', file=file)
+                print(f'  train_loss: {train_loss:.4f}', file=file)
+                print(f'  train_rmse: {train_rmse:.4f}', file=file)
+                print(f'  test_loss: {test_loss:.4f}', file=file)
+                print(f'  test_rmse: {test_rmse:.4f}', file=file)
+                print(file=file)
+            
+            ckpt = dict(
+                epoch=epoch+1,
+                train_loss=train_loss_list,
+                train_rmse=train_rmse_list,
+                test_loss=test_loss_list,
+                test_rmse=test_rmse_list,
+                head={
+                    key: val.clone().detach().cpu()
+                    for key, val in head.state_dict().items()
+                },
+            )
+            if test_rmse < best_rmse:
+                best_rmse = test_rmse
+                torch.save(ckpt, str(best_filename))
+            torch.save(ckpt, str(last_filename))
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser('lineval.nyud')
+    init_parser(parser, defaults=dict(epochs=1000))
+    args = parser.parse_args()
+    
+    rank = int(os.environ['LOCAL_RANK'])
+    world_size = int(os.environ['WORLD_SIZE'])
+    os.environ['IS_MASTER_NODE'] = str(int(rank == 0))
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
+    
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        dist.destroy_process_group()
