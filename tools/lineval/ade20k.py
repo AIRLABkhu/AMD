0cf655241d068c8611cc3933f9dfa846a8b93234
nda111 <geunhyeok@khu.ac.kr>
2025-05-14 06:53:21 +0000

    Revert ADE-20K linear evaluation to the flawless version. Revert ADE-20K linear evaluation to the flawless version.
    On branch master
    Your branch is behind 'origin/master' by 2 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   tools/lineval/ade20k.py

--- tools/lineval/ade20k.py
+++ tools/lineval/ade20k.py
@@ -3,13 +3,13 @@ from argparse import ArgumentParser, Namespace
 from tqdm.auto import tqdm
 
 import einops
-from torchmetrics.classification import MulticlassJaccardIndex
 
 import torch
 from torch import nn, optim
 import torch.nn.functional as F
 from torch.optim import lr_scheduler
 from torch import distributed as dist
+from torchmetrics.classification import MulticlassJaccardIndex
 
 from mdistiller.dataset.ade20k import get_ade20k_dataloaders
 from mdistiller.models._base import ModelBase
@@ -26,7 +26,7 @@ from mdistiller.utils import dist_fn
 # Utility
 
 class SemSegHead(nn.Module):
-    def __init__(self, embed_dim: int, upsample_factor: int = 4, num_classes: int = 150):
+    def __init__(self, embed_dim: int, upsample_factor: int=4, num_classes: int=150):
         super().__init__()
         self.upsample_factor = upsample_factor
         self.num_classes = num_classes
@@ -34,24 +34,22 @@ class SemSegHead(nn.Module):
 
     def forward(self, feat: torch.Tensor):
         """
-        feat: (B, N+1, C), ViT final layer output with CLS token
+        feat: (B, N+?, C), ViT final layer output with CLS token
         Returns:
             pred_logit: (B, num_bins, H, W)
         """
-        num_rows = int(feat.size(1)**0.5)
-        patch_tokens = feat[:, -num_rows*num_rows:]  # (B, N, C) 
-        patch_tokens = einops.rearrange(
-            patch_tokens, 'B (H W) C -> B C H W', H=num_rows
-        )
+        resolution = int(feat.size(1) ** 0.5)
+        feat = feat[:, -resolution*resolution:]  # (B, N, C)
 
+        feat = einops.rearrange(feat, 'B (H W) C -> B C H W', H=resolution)
+        logit = self.head(feat)  # (B, cls, H, W)
         upsampled = F.interpolate(
-            patch_tokens,
+            logit,
             scale_factor=self.upsample_factor,
             mode='bilinear',
             align_corners=False,
-        )  # (B, C, H*16, W*16)
-        pred = self.head(upsampled)  # (B, num_bins, H', W')
-        return pred
+        )  # (B, cls, H*r, W*r)
+        return upsampled
 
 
 def main(args: Namespace):
@@ -64,7 +62,7 @@ def main(args: Namespace):
         _, log_filename, best_filename, last_filename = prepare_lineval_dir(
             args.expname, 
             tag=str(args.tag), 
-            dataset='ade20k', 
+            dataset='nyud', 
             args=vars(args)
         )
     
@@ -74,17 +72,19 @@ def main(args: Namespace):
         args.num_workers, use_ddp=True,
     )
     if args.timm_model is not None:
-        if IS_MASTER:
-            print(f'Loading {args.timm_model} from timm')
+        print(f"Loading {args.timm_model} from timm")
         model = imagenet_model_dict[args.timm_model](pretrained=True)
     else:
         model, _ = load_from_checkpoint(args.expname, tag=args.tag)
     model: ModelBase = model.cuda(DEVICE)
     head = SemSegHead(model.embed_dim, upsample_factor=16).cuda(DEVICE)
     
-    miou = MulticlassJaccardIndex(150, ignore_index=-1).to(DEVICE)
+    for param in head.parameters():
+        param.data = param.data.contiguous()
+    
     model = nn.parallel.DistributedDataParallel(model, device_ids=[rank], find_unused_parameters=True)
     head = nn.parallel.DistributedDataParallel(head, device_ids=[rank])
+    miou = MulticlassJaccardIndex(150, ignore_index=-1).cuda(rank)
     optimizer = optim.SGD(
         head.parameters(),
         lr=args.learning_rate,
@@ -98,60 +98,64 @@ def main(args: Namespace):
     
     # Training Loop
     best_miou = -torch.inf
-    train_loss_list, train_miou_list, test_loss_list, test_miou_list = [], [], [], []
+    train_loss_list, train_miou_list, test_loss_list, test_imou_list = [], [], [], []
     for epoch in range(args.epochs):
         
         with tqdm(train_loader, desc=f'TRAIN {epoch+1}', dynamic_ncols=True, disable=not IS_MASTER) as bar:
-            total_loss, total_miou, total = 0, 0, 0
+            total_loss, total_miou, total, total_classes = 0, 0, 0, 0
             for input, target in bar:
+                target = target.long()
                 with torch.no_grad():
                     x = model.forward(input.cuda(DEVICE))[1]['feats'][-1]
-                pred_logit = head(x) # (B, 150, H, W)
+                pred_logit = head.forward(x)  # (B, 150, H, W)
 
-                loss = F.cross_entropy(pred_logit, target.to(DEVICE), ignore_index=-1)
+                loss = F.cross_entropy(pred_logit, target.cuda(DEVICE), ignore_index=-1)
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
                 scheduler.step()
                 
                 with torch.no_grad():
-                    loss = F.cross_entropy(pred_logit, target.to(DEVICE), reduction='none', ignore_index=-1)
+                    loss = torch.nn.functional.cross_entropy(pred_logit, target.to(DEVICE), ignore_index=-1, reduction='none')
                     loss_all = dist_fn.gather(loss)
-                    local_miou = miou(pred_logit, target.to(DEVICE))
-                    miou_all = dist_fn.gather(local_miou[None])
+                    local_miou = miou(pred_logit, target.to(DEVICE))[None]
+                    miou_all = dist_fn.gather(local_miou)
                     
-                    total_loss += loss_all.sum().cpu().item()
+                    total_loss += loss_all.mean().cpu().item()
                     total_miou += miou_all.sum().cpu().item()
                     total += loss_all.size(0)
+                    total_classes += miou_all.numel()
                     
                     if IS_MASTER:
                         bar.set_postfix(dict(
                             lr=optimizer.param_groups[0]['lr'],
                             loss=total_loss/total,
-                            miou=total_miou/total*100.0,
+                            miou=f'{total_miou/total_classes*100:.2f}%',
                         ))
-            train_miou = total_miou / total
+            train_miou = total_miou / total_classes
             train_loss = total_loss / total
 
         with tqdm(test_loader, desc=f' TEST {epoch+1}', dynamic_ncols=True, disable=not IS_MASTER) as bar, torch.no_grad():
-            total_loss, total_miou, total = 0, 0, 0
+            total_loss, total_miou, total, total_classes = 0, 0, 0, 0
             for input, target in bar:
+                target = target.long()
                 x = model.forward(input.cuda(DEVICE))[1]['feats'][-1]
-                pred_logit = head(x) # (B, 150, H, W)
+                pred_logit = head.forward(x) # (B, 150, H, W)
                 
-                loss = F.cross_entropy(pred_logit, target.to(DEVICE), reduction='none', ignore_index=-1)
+                loss = torch.nn.functional.cross_entropy(pred_logit, target.to(DEVICE), ignore_index=-1, reduction='none')
                 loss_all = dist_fn.gather(loss)
-                local_miou = miou(pred_logit, target.to(DEVICE))
-                miou_all = dist_fn.gather(local_miou[None])
+                local_miou = miou(pred_logit, target.to(DEVICE))[None]
+                miou_all = dist_fn.gather(local_miou)
                 
-                total_loss += loss_all.sum().cpu().item()
+                total_loss += loss_all.mean().cpu().item()
                 total_miou += miou_all.sum().cpu().item()
                 total += loss_all.size(0)
+                total_classes += miou_all.numel()
                 
                 if IS_MASTER:
                     bar.set_postfix(dict(
                         loss=total_loss/total,
-                        miou=total_miou/total*100.0,
+                        miou=f'{total_miou/total_classes*100:.2f}%',
                     ))
             test_miou = total_miou / total
             test_loss = total_loss / total
@@ -160,23 +164,23 @@ def main(args: Namespace):
         train_loss_list.append(train_loss)
         train_miou_list.append(train_miou)
         test_loss_list.append(test_loss)
-        test_miou_list.append(test_miou)
+        test_imou_list.append(test_miou)
         
         if IS_MASTER:
             with open(log_filename, 'a') as file:
                 print(f'- epoch: {epoch+1}', file=file)
                 print(f'  train_loss: {train_loss:.4f}', file=file)
-                print(f'  train_miou: {train_miou*100.0:.2f}%', file=file)
+                print(f'  train_miou: {train_miou*100:.4f} %', file=file)
                 print(f'  test_loss: {test_loss:.4f}', file=file)
-                print(f'  test_miou: {test_miou*100.0:.2f}%', file=file)
+                print(f'  test_miou: {test_miou*100:.4f} %', file=file)
                 print(file=file)
             
             ckpt = dict(
                 epoch=epoch+1,
                 train_loss=train_loss_list,
-                train_miou=train_miou_list,
+                train_rmse=train_miou_list,
                 test_loss=test_loss_list,
-                test_miou=test_miou_list,
+                test_rmse=test_imou_list,
                 head={
                     key: val.clone().detach().cpu()
                     for key, val in head.state_dict().items()
